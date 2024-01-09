1442ef74e73cd891f8cbd099b5029574dee40fb2
MarcosYonamine963 <marcos.m.yonamine@gmail.com>
2024-01-09 16:19:55 -0300

    added micro delay lib

---
+++ G070/Basics/MicroDelay/Core/Src/micro_delay.c
@@ -0,0 +1,36 @@
+/*
+ * micro_delay.c
+ *
+ *  Created on: Jan 9, 2024
+ *      Author: mcmachine
+ */
+
+
+#include "micro_delay.h"
+#include "stm32g070xx.h"
+
+
+uint32_t micro_get_val()
+{
+    uint32_t ms;
+    uint32_t st;
+    do
+    {
+        ms = micro_update_millis;
+        st = SysTick->VAL;
+        asm volatile("nop");
+        asm volatile("nop");
+    } while (ms != micro_update_millis);
+    return ms * 1000 - st / ((SysTick->LOAD + 1) / 1000);
+}
+
+
+uint32_t micro_get_val_from_ISR()
+{
+    uint32_t st = SysTick->VAL;
+    uint32_t pending = SCB->ICSR & SCB_ICSR_PENDSTSET_Msk;
+    uint32_t ms = micro_update_millis;
+    if (pending == 0)
+        ms++;
+    return ms * 1000 - st / ((SysTick->LOAD + 1) / 1000);
+}
